dfbc10f25bcea7e34304cebfa44f49863d00ebe3
Blitz-Cloud <ionutp626@gmail.com>
2025-05-06 15:44:05 +0300

    added server code

--- arduino/src/main.cpp
+++ arduino/src/main.cpp
@@ -1,8 +1,17 @@
+#include <time.h>
+#include <string.h>
+#include <Stream.h>
+#include <OneWire.h>
 #include <Arduino.h>
+// #include <ArduinoJson.h>
 #include <SoftwareSerial.h>
+#include <DallasTemperature.h>
 #include <ArduinoHttpClient.h>
 #include <BlitzCloudDisplayController.h>
 
+void (*resetBoard)(void) = 0;
+void connectSimToNetwork();
+// SIM AND HTTP CLIENT
 #define TINY_GSM_MODEM_SIM800
 #include <TinyGsmClient.h>
 
@@ -21,39 +30,79 @@ TinyGsm modem(sim800l);
 TinyGsmClient client(modem);
 HttpClient httpClient(client, "130.61.92.168", 8030);
 int *gpio = nullptr;
+
+// Display
 DisplayLed2Digits display(6, 5, 4, 1, gpio, 7, 8);
+
+// TEMPERATURA
+
+// Data wire is conntec to the Arduino digital pin 4
+#define ONE_WIRE_BUS 12
+
+// Setup a oneWire instance to communicate with any OneWire devices
+OneWire oneWire(ONE_WIRE_BUS);
+
+// Pass our oneWire reference to Dallas Temperature sensor
+DallasTemperature sensors(&oneWire);
 
 void setup()
 {
-
-  // display.showNumber(1234, 2000);
-  char content[5];
-  strcpy(content, "c1");
-  display.showCharacter(content, 3000);
-
   Serial.begin(9600);
+  sensors.begin();
   Serial.println("Wait...");
-
-  // TinyGsmAutoBaud(sim800l, 9600, 115200);
-
   sim800l.begin(9600);
   delay(6000);
-
   Serial.println("Initializing modem...");
   modem.restart();
-  // modem.init();
-
-  // String modemInfo = modem.getModemInfo();
-  // Serial.print("Modem Info: ");
-  // Serial.println(modemInfo);
+  String modemInfo = modem.getModemInfo();
+  Serial.print("Modem Info: ");
+  Serial.println(modemInfo);
+  if (modemInfo.length() == 0)
+  {
+    display.showCharacter("ES0", 3);
+    resetBoard();
+  }
 }
 
 void loop()
 {
+  unsigned int start = millis();
+
+  connectSimToNetwork();
+  String time = modem.getGSMDateTime(DATE_TIME);
+  Serial.println(time);
+  Serial.println("Making POST request");
+  String contentType = "application/json";
+  char data[128];
+  sensors.requestTemperatures();
+  float tempInside = sensors.getTempCByIndex(0);
+  float tempOutside = 0.0;
+  sprintf(data, "{\"tempInside\":%d.%d,\"tempOutside\":%d.%d,\"time\":\"%s\",\"name\":\"Arduino/V1.0\"}", int(tempInside), (int(tempInside * 100) % 100), int(tempOutside), (int(tempOutside * 100) % 100), time.c_str());
+  Serial.println(data);
+  String testPayload = "testdata";
+  httpClient.post("/", contentType, data);
+  int statusCode = httpClient.responseStatusCode();
+  String response = httpClient.responseBody();
+
+  Serial.print("Status code: ");
+  Serial.println(statusCode);
+  Serial.print("Response: ");
+  Serial.println(response);
+  Serial.println("Wait five seconds");
+  Serial.print("Delta time:");
+  Serial.println(millis() - start);
+
+  delay(5000);
+}
+
+void connectSimToNetwork()
+{
+
   Serial.print("Waiting for network...");
   if (!modem.waitForNetwork())
   {
     Serial.println(" fail");
+    display.showCharacter("ES1", 3);
     delay(10000);
     return;
   }
@@ -68,6 +117,7 @@ void loop()
   if (!modem.gprsConnect("broadband", "", ""))
   {
     Serial.println(" fail");
+    display.showCharacter("ES2", 3);
     delay(10000);
     return;
   }
@@ -77,91 +127,4 @@ void loop()
   {
     Serial.println("GPRS connected");
   }
-  Serial.print(F("Performing HTTP GET request... "));
-  int err = httpClient.get("/");
-  if (err != 0)
-  {
-    Serial.println(F("failed to connect"));
-    delay(10000);
-    return;
-  }
-
-  int status = httpClient.responseStatusCode();
-  Serial.print(F("Response status code: "));
-  Serial.println(status);
-  if (!status)
-  {
-    delay(10000);
-    return;
-  }
-
-  Serial.println(F("Response Headers:"));
-  while (httpClient.headerAvailable())
-  {
-    String headerName = httpClient.readHeaderName();
-    String headerValue = httpClient.readHeaderValue();
-    Serial.println("    " + headerName + " : " + headerValue);
-  }
-
-  int length = httpClient.contentLength();
-  if (length >= 0)
-  {
-    Serial.print(F("Content length is: "));
-    Serial.println(length);
-  }
-  if (httpClient.isResponseChunked())
-  {
-    Serial.println(F("The response is chunked"));
-  }
-
-  String body = httpClient.responseBody();
-  Serial.println(F("Response:"));
-  Serial.println(body);
-
-  Serial.print(F("Body length is: "));
-  Serial.println(body.length());
-
-  // Shutdown
-
-  httpClient.stop();
-  Serial.println(F("Server disconnected"));
 }
-
-// void setUpSim()
-// {
-
-//   sim800l.println("AT"); // Once the handshake test is successful, it will back to OK
-//   updateSerial();
-//   sim800l.println("AT+CSQ"); // Signal quality test, value range is 0-31 , 31 is the best
-//   updateSerial();
-//   sim800l.println("AT+CCID"); // Read SIM information to confirm whether the SIM is plugged
-//   updateSerial();
-//   sim800l.println("AT+CREG?"); // Check whether it has registered in the network
-//   updateSerial();
-//   sim800l.println("AT+CFUN=1");
-//   updateSerial();
-//   sim800l.println("AT+CPIN?");
-//   updateSerial();
-//   sim800l.println("AT+CGATT=1");
-//   updateSerial();
-
-//   sim800l.println("AT+CSTT=\"broadband\",\"\",\"\"");
-//   updateSerial();
-//   sim800l.println("AT+CIICR");
-//   updateSerial();
-//   sim800l.println("AT+CIFSR");
-//   updateSerial();
-// }
-
-// void updateSerial()
-// {
-//   delay(500);
-//   while (Serial.available())
-//   {
-//     sim800l.write(Serial.read()); // Forward what Serial received to Software Serial Port
-//   }
-//   while (sim800l.available())
-//   {
-//     Serial.write(sim800l.read()); // Forward what Software Serial received to Serial Port
-//   }
-// }
